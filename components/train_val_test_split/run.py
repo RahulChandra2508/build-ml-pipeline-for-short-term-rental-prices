07dc8d6ea761a544cc69996e0982109bef516054
Rahul Chandra <rahulchandra1918@gmail.com>
2023-05-01 18:24:19 +0530

    completed train_val_test_split

--- components/train_val_test_split/run.py
+++ components/train_val_test_split/run.py
@@ -8,7 +8,7 @@ import pandas as pd
 import wandb
 import tempfile
 from sklearn.model_selection import train_test_split
-from wandb_utils.log_artifact import log_artifact
+# from wandb_utils.log_artifact import log_artifact
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
 logger = logging.getLogger()
@@ -33,21 +33,34 @@ def go(args):
         random_state=args.random_seed,
         stratify=df[args.stratify_by] if args.stratify_by != 'none' else None,
     )
+    
+    trainval.to_csv("train_dataset.csv", index= False)
+    test.to_csv("test_dataset.csv", index= False)
 
     # Save to output files
-    for df, k in zip([trainval, test], ['trainval', 'test']):
+    for df, k in zip(["train_dataset.csv", 'test_dataset.csv'], ['trainval', 'test']):
         logger.info(f"Uploading {k}_data.csv dataset")
-        with tempfile.NamedTemporaryFile("w") as fp:
+        # with tempfile.NamedTemporaryFile("w") as fp:
 
-            df.to_csv(fp.name, index=False)
+        #     df.to_csv(fp.name, index=False)
 
-            log_artifact(
-                f"{k}_data.csv",
-                f"{k}_data",
-                f"{k} split of dataset",
-                fp.name,
-                run,
-            )
+        artifact = wandb.Artifact(
+            name=  f"{k}_data.csv",
+            type= f"{k}_data",
+            description= f"{k} split of dataset"
+        )
+
+        artifact.add_file(df)
+        run.log_artifact(artifact)
+
+            # log_artifact(
+            #     f"{k}_data.csv",
+            #     f"{k}_data",
+            #     f"{k} split of dataset",
+            #     fp.name,
+            #     run,
+            # )
+    run.finish()
 
 
 if __name__ == "__main__":
